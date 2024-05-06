891da4a52d4ceabb4065d63968c2a3d9c7a322a3
Ahmed Radwan <70094970+Ahmed01126@users.noreply.github.com>
2024-05-06 14:59:51 +0300

    task 2

--- src/Main.java
+++ src/Main.java
@@ -2,81 +2,31 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("What is your name?");
         Scanner s = new Scanner(System.in);
-        String name = s.next();
-        System.out.println("What time of day are we?");
-        String time = s.next();
-        System.out.println("How old are you?");
-        byte age = s.nextByte();
+        System.out.println("Hi Sir,\tPlease enter your money?");
+        float initialMoney;
+        initialMoney = s.nextFloat();
 
-        System.out.println("Which do you prefer performance or clean code?");
-        System.out.println("P : performance  |  C : clean code");
-        char c = s.next().charAt(0);
-        if (c == 'P') {
-            /* ******************************************** */
-            /* *************** Performance **************** */
-            /* ******************************************** */
+        System.out.println("What is the bank interest rate?");
+        float interestRate;
+        interestRate = s.nextFloat();
+        interestRate /= 100;
 
-            // Option 1
-            if (time.equals("morning") || time.equals("evening")) {
-                System.out.print("Good " + time + ", ");
-            } else {
-                System.out.println("Hi, ");
-            }
+        System.out.println("What is the money limit?");
+        float limit;
+        limit = s.nextFloat();
 
-            if (age <= 0) {
-                System.out.println("Wrong age!");
-            } else if (age > 30) {
-                System.out.println("you are getting older " + name + '.');
-            } else if (age > 20) {
-                System.out.println("you are young " + name + '.');
-            } else {
-                System.out.println("you are very young " + name + '.');
-            }
-
-            // Option 2
-            if (age <= 0) {
-                System.out.println("Wrong age!");
-            } else {
-                if (time.equals("morning") || time.equals("evening")) {
-                    System.out.print("Good " + time + ", ");
-                } else {
-                    System.out.print("Hi, ");
-                }
-
-                if (age > 30) {
-                    System.out.println("you are getting older " + name + '.');
-                } else if (age > 20) {
-                    System.out.println("you are young " + name + '.');
-                } else {
-                    System.out.println("you are very young " + name + '.');
-                }
-            }
-        } else {
-            /* ******************************************** */
-            /* *************** Clean Code ***************** */
-            /* ******************************************** */
-            // Option 1
-
-            boolean checkTime = time.equals("morning");
-            checkTime |= time.equals("evening");
-            //boolean checkTime = time.equals("morning") || time.equals("evening");
-            if (checkTime) {
-                System.out.print("Good " + time + ", ");
-            } else {
-                System.out.print("Hi, ");
-            }
-
-            if (age > 0 && age <= 20) {
-                System.out.println("you are very young " + name + '.');
-            } else if (age > 20 && age <= 30) {
-                System.out.println("you are young " + name + '.');
-            } else if (age > 30) {
-                System.out.println("you are getting older " + name + '.');
-            } else {
-                System.out.println("Wrong age!");
+        byte years = 0;
+        float money = initialMoney;
+        while (money < limit) {
+            years++;
+            money += money * interestRate;
+            if (years == 5) {
+                money += initialMoney * interestRate;
             }
         }
+        System.out.println("The number of years: " + years + '\n' + "Your Balance:        " + money);
+
+
     }
 }
